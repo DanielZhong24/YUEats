00459b64e6651504cd10fa7e31d947be21e68e3a
DanielZhong24 <danielzhong0618@gmail.com>
2025-11-17 14:13:04 -0500

    feat: spring boot framework set up with necessary dependencies in pom.xml

---
+++ src/main/java/cssd2101/yueats/YuEatsApplication.java
@@ -0,0 +1,13 @@
+package cssd2101.yueats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuEatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cssd2101/yueats/YuEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package cssd2101.yueats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuEatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
