f972090ed8d11ac1b8cc806ad18188b93983bc6b
DanielZhong24 <danielzhong0618@gmail.com>
2025-11-18 15:47:04 -0500

    feat: successful build of the maven project and h2 initialization

--- src/main/java/cssd2101/yueats/model/Customer.java
+++ src/main/java/cssd2101/yueats/model/Customer.java
@@ -1,6 +1,18 @@
 package cssd2101.yueats.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+
+@Entity
+@DiscriminatorValue("CUSTOMER")
 public class Customer extends User {
+
+    @Column(nullable = true)
+    private String studentNumber;
+
 
 
 
