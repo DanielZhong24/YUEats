26fb9778d8a9ae8a75199e189aab0fccf58862ec
DanielZhong24 <danielzhong0618@gmail.com>
2025-11-18 15:47:24 -0500

    feat: first testing endpoint established

--- src/main/java/cssd2101/yueats/YuEatsApplication.java
+++ src/main/java/cssd2101/yueats/YuEatsApplication.java
@@ -2,6 +2,7 @@ package cssd2101.yueats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class YuEatsApplication {
@@ -9,5 +10,6 @@ public class YuEatsApplication {
     public static void main(String[] args) {
         SpringApplication.run(YuEatsApplication.class, args);
     }
+
 
 }
