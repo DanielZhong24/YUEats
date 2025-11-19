c3158d3916e806ebdfea63099b9b6e58a5a3d11b
DanielZhong24 <danielzhong0618@gmail.com>
2025-11-19 15:22:03 -0500

    feat: endpoint /user to fetch all users

--- src/main/java/cssd2101/yueats/controller/UserController.java
+++ src/main/java/cssd2101/yueats/controller/UserController.java
@@ -1,13 +1,21 @@
 package cssd2101.yueats.controller;
 
+import cssd2101.yueats.model.Customer;
+import cssd2101.yueats.model.User;
+import cssd2101.yueats.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
+    @GetMapping("/user")
+    public List<User> index() {
+        return (List<User>) userRepository.findAll();
     }
 }
