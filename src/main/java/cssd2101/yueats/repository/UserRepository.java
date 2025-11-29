2b4d9151c8f8aa9b76b992091710caa9a34459b1
DanielZhong24 <danielzhong0618@gmail.com>
2025-11-28 22:25:09 -0500

    fix: made UserRepository extend JpaRepository instead of CrudRepository for more functionality. Added existsByEmail abstract function

--- src/main/java/cssd2101/yueats/repository/UserRepository.java
+++ src/main/java/cssd2101/yueats/repository/UserRepository.java
@@ -2,9 +2,11 @@ package cssd2101.yueats.repository;
 
 import cssd2101.yueats.model.Customer;
 import cssd2101.yueats.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface UserRepository extends CrudRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 
+    boolean existsByEmail(String email);
 }
