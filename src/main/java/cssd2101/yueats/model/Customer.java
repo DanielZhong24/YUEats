687664fc1973dd3cc573a0c670483638f38958b8
DanielZhong24 <danielzhong0618@gmail.com>
2025-11-28 22:32:00 -0500

    feat: annotations for building auto getters and setters

--- src/main/java/cssd2101/yueats/model/Customer.java
+++ src/main/java/cssd2101/yueats/model/Customer.java
@@ -1,14 +1,18 @@
 package cssd2101.yueats.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @DiscriminatorValue("CUSTOMER")
+@SuperBuilder
+@Getter
+@Setter
+@NoArgsConstructor
 public class Customer extends User {
-
-
-
 
 }
