74e65acf52b6987dc1edd1748fbce42a61ab37da
DanielZhong24 <danielzhong0618@gmail.com>
2025-11-28 22:27:58 -0500

    feat(api): remapped endpoints for user controller

--- src/main/java/cssd2101/yueats/controller/UserController.java
+++ src/main/java/cssd2101/yueats/controller/UserController.java
@@ -5,16 +5,18 @@ import cssd2101.yueats.model.User;
 import cssd2101.yueats.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/user")
+    @GetMapping("/getUsers")
     public List<User> index() {
         return (List<User>) userRepository.findAll();
     }
