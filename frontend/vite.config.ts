26eb5cd0a28ed8cba57ae9dfdeb9e5465f9c539f
Anthony Toyco <amieltoyco@gmail.com>
2025-12-03 06:12:26 +0000

    fix(frontend): cleaned frontend dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,9 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config https://vitest.dev/config
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: true,
-    port: 5173
-  },
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
-  }
-})
+  plugins: [react(), tailwindcss()],
+  test: {},
+});
