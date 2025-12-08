610bd93fc1144294132d937730df413ea1bc4ed1
DanielZhong24 <danielzhong0618@gmail.com>
2025-12-08 16:44:08 -0500

    fix(menu): type conversion change

--- backend/src/main/java/cssd2101/yueats/model/MenuItem.java
+++ backend/src/main/java/cssd2101/yueats/model/MenuItem.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Entity
 @Data
 @Builder
@@ -29,5 +31,5 @@ public class MenuItem {
     private String description;
 
     @Column(name = "price")
-    private Double price;
+    private BigDecimal price;
 }
