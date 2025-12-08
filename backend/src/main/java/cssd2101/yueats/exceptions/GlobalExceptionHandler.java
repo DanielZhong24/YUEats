dc8c6a8d0c7afd3e0b558df0d3a9c1b03c542ea5
DanielZhong24 <danielzhong0618@gmail.com>
2025-12-08 16:43:35 -0500

    fix(exception): throw a bad request instead of internal server error because it didn't match with the actual output

--- backend/src/main/java/cssd2101/yueats/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/cssd2101/yueats/exceptions/GlobalExceptionHandler.java
@@ -43,6 +43,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
