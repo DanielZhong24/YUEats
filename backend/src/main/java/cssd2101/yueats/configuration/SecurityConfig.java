bb698ec5ddfebe648eea3806b11477edc56702f1
Andrew Cao <andrewcao618@gmail.com>
2025-12-09 19:28:00 -0500

    Added authentication provider

--- backend/src/main/java/cssd2101/yueats/configuration/SecurityConfig.java
+++ backend/src/main/java/cssd2101/yueats/configuration/SecurityConfig.java
@@ -2,16 +2,24 @@ package cssd2101.yueats.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public BCryptPasswordEncoder encoder() {
@@ -32,12 +40,12 @@ public class SecurityConfig {
     }
     */
 
-    /*
+
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(encoder());
         return provider;
     }
-    */
+
 }
---
+++ backend/src/main/java/cssd2101/yueats/service/CustomerDetailsService.java
@@ -0,0 +1,29 @@
+package cssd2101.yueats.service;
+
+import cssd2101.yueats.model.User;
+import cssd2101.yueats.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomerDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+
+        return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+                .password(user.getPasswordHash())
+                .roles(user.getUserRole().name())
+                .build();
+    }
+}
