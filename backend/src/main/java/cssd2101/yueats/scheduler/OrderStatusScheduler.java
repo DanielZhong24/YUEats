a819ae277b2e24a0088bdaa3f7fa6e47bd033b20
Andrew Cao <andrewcao618@gmail.com>
2025-12-12 20:08:09 -0500

    Removed restaurant repo in scheduler

--- backend/src/main/java/cssd2101/yueats/scheduler/OrderStatusScheduler.java
+++ backend/src/main/java/cssd2101/yueats/scheduler/OrderStatusScheduler.java
@@ -2,22 +2,17 @@ package cssd2101.yueats.scheduler;
 
 import cssd2101.yueats.model.Order;
 import cssd2101.yueats.repository.OrderRepository;
-import cssd2101.yueats.repository.RestaurantRepository;
 import cssd2101.yueats.types.OrderStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class OrderStatusScheduler {
-    private final RestaurantRepository restaurantRepository;
     private final OrderRepository orderRepository;
     private final OrderStateMachine statusMachine;
 
-    public OrderStatusScheduler(RestaurantRepository restaurantRepository, OrderRepository orderRepository, OrderStateMachine statusMachine) {
-        this.restaurantRepository = restaurantRepository;
+    public OrderStatusScheduler(OrderRepository orderRepository, OrderStateMachine statusMachine) {
         this.orderRepository = orderRepository;
         this.statusMachine = statusMachine;
     }
