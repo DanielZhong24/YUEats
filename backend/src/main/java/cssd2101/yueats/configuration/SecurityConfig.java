8665e0eeb6545bf691cf4557913cb74116ee3194
DanielZhong24 <danielzhong0618@gmail.com>
2025-12-18 17:58:31 -0500

    fix(cors): adding rules in application.properties to allow same source

--- backend/src/main/java/cssd2101/yueats/configuration/SecurityConfig.java
+++ backend/src/main/java/cssd2101/yueats/configuration/SecurityConfig.java
@@ -109,7 +109,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                Arrays.asList("http://localhost:3000", "https://yueats-frontend-production.up.railway.app/"));
+                Arrays.asList("http://localhost:3000", "https://yueats-frontend-production.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
