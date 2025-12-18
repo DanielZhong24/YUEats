4712cf52771e7cbf2e89550ca025175a3f9a1071
DanielZhong24 <danielzhong0618@gmail.com>
2025-12-18 17:53:10 -0500

    fix(cors)!: added prod url for cors

--- backend/src/main/java/cssd2101/yueats/configuration/SecurityConfig.java
+++ backend/src/main/java/cssd2101/yueats/configuration/SecurityConfig.java
@@ -108,7 +108,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://localhost:3000", "https://yueats-frontend-production.up.railway.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
